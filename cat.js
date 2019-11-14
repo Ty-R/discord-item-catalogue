f7cf9a5b092cc444cd59ed694b6c776050f19c48
TyRoberts <t.roberts@livelinktechnology.net>
2019-11-14 18:30:45 +0000

    Add ability to remove items

--- cat.js
+++ cat.js
@@ -11,10 +11,11 @@ const bot = new Discord.Client({
 
 const botUsername = 'Catalogue';
 const catalogueFile = 'catalogue.json';
+const writeActions = ['add', 'remove'];
 let catalogue;
 
 if (!fs.existsSync(catalogueFile)) {
-  fs.writeFile(catalogueFile, '{}', (err) => {
+  fs.writeFile(catalogueFile, '{"listings": []}', (err) => {
     if (err) {
       console.log(err);
       process.exit();
@@ -35,17 +36,24 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       return;
     }
 
+    if (writeActions.includes(action) && !args.item) {
+      reply(channelID, `Calling \`${action}\` requires an item. See \`!cat help\` for more information `);
+      return;
+    }
+
     switch(action) {
       case 'add':
-        addItemToCatalogue(user, args);
-        reply(channelID, `Hi, ${user}! I've added **${toTitleCase(args.item)}** to the catalogue for you`);
+        reply(channelID, `Hi, ${user}! ${addItemToCatalogue(user, args)}`);
+        break;
+      case 'remove':
+        reply(channelID, `Hi, ${user}! ${removeItemFromCatalogue(user, args)}`);
         break;
       case 'search':
         const { resultCount, results } = botSearchResults(searchCatalogue(args));
         reply(channelID, `Hi, ${user}! That query returned ${pluralize('result', resultCount, true)} \n\n${results}`);
         break;
       default:
-        reply(channelID, "I don't understand that. Type `!cat help` for more information");
+        reply(channelID, "I don't understand that. See `!cat help` for more information");
     }
   }  
 });
@@ -54,30 +62,6 @@ function reply(channelID, message) {
   bot.sendMessage({
     to: channelID,
     message: message
-  });
-}
-
-function sendCustomHelpMessage(channelID) {
-  bot.sendMessage({
-    to: channelID,
-    message: "Here's some basic information about me",
-    embed: {
-      "description":"Catalogue is a bot designed to make it easier to keep track of what is being sold, by allowing users to _add_ items to it, and query existing items within it. Click [here](https://github.com/TyRoberts/discord-item-catalogue) for more information.",
-      "color":3447003,
-      "thumbnail": {
-        "url": "https://gamepedia.cursecdn.com/minecraft_gamepedia/8/85/Knowledge_book.png?version=0c9d97dd48215c6faa9e4513f5d87aa8"
-      },
-      "fields": [
-        {
-          "name": "Example: Adding an item to the catalogue",
-          "value": "`!cat add item: diamond`"
-        },
-        {
-          "name": "Example: Searching for an item in the catalogue",
-          "value": "`!cat search item: diamond`"
-        }
-      ]
-    }
   });
 }
 
@@ -93,6 +77,14 @@ function updateLocalCatalogue() {
 }
 
 function addItemToCatalogue(user, args) {
+  const existing = searchCatalogue({seller: user, item: args.item}).length > 0;
+
+  if (existing) return `You're already have a **${args.item}** listing.`;
+  createNewCatalogueEntry(user, args);
+  return `I've added **${toTitleCase(args.item)}** to the catalogue for you`;
+}
+
+function createNewCatalogueEntry(user, args) {
   const newListing = {
     seller: user,
     quantity: args.quantity,
@@ -102,6 +94,22 @@ function addItemToCatalogue(user, args) {
   }
 
   catalogue.listings.push(newListing);
+  updateLocalCatalogue();
+}
+
+function removeItemFromCatalogue(user, args) {
+  const listing = searchCatalogue({seller: user, item: args.item})[0];
+  if (listing) {
+    deleteCatalogueEntry(listing);
+    return `I've removed your **${args.item}** listing`;
+  }
+
+  return `I couldn't find a **${args.item}** that belongs to you.`;
+}
+
+function deleteCatalogueEntry(listing) {
+  const index = catalogue.listings.indexOf(listing);
+  catalogue.listings.splice(index, 1)
   updateLocalCatalogue();
 }
 
@@ -151,14 +159,6 @@ function searchCatalogue(args) {
   return currentListings;
 }
 
-function helpCommands() {
-  return [
-    'Here are the list of commands I understand:',
-    '• To **add** an item to the catalogue: `!cat add [filters]`',
-    '• To **search** the catalogue: `!cat search [filters]`'
-  ].join("\n");
-}
-
 function parseInput(message) {
   const parsedArgs = {};
   const knownArgs = ['item', 'quantity', 'price', 'location', 'seller'];
@@ -180,9 +180,37 @@ function toTitleCase(str) {
 }
 
 function toSafeString(str) {
-  return str.replace(/[^ \w]+/g, '').trim();
+  return str.replace(/[^ \w]+/g, '').trim().toLowerCase();
 }
 
 function loadCatalogue() {
   catalogue = JSON.parse(fs.readFileSync(catalogueFile));
+}
+
+function sendCustomHelpMessage(channelID) {
+  bot.sendMessage({
+    to: channelID,
+    message: "Here's some basic information about me",
+    embed: {
+      "description":"Catalogue is a bot designed to make it easier to keep track of what is being sold, by allowing users to _add_ items to it, and query existing items within it. Click [here](https://github.com/TyRoberts/discord-item-catalogue) for more information.",
+      "color":3447003,
+      "thumbnail": {
+        "url": "https://gamepedia.cursecdn.com/minecraft_gamepedia/8/85/Knowledge_book.png?version=0c9d97dd48215c6faa9e4513f5d87aa8"
+      },
+      "fields": [
+        {
+          "name": "Example: Adding an item to the catalogue",
+          "value": "`!cat add item: diamond`"
+        },
+        {
+          "name": "Example: Removing an item in the catalogue",
+          "value": "`!cat remove item: diamond`"
+        },
+        {
+          "name": "Example: Searching for an item in the catalogue",
+          "value": "`!cat search item: diamond`"
+        }
+      ]
+    }
+  });
 }
