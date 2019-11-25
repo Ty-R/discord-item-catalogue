36bf62137c5b52f3f13236a2a1e4574a77e37287
TyRoberts <t.roberts@livelinktechnology.net>
2019-11-25 08:29:59 +0000

    .

--- logger.js
+++ logger.js
@@ -1,5 +1,8 @@
 const winston = require('winston');
 
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, prettyPrint } = format;
+
 // https://www.npmjs.com/package/winston#further-reading
 //
 // In order to use this same logger configuration across
@@ -7,7 +10,15 @@ const winston = require('winston');
 // Once this is created then any modules that require
 // winston will automatically use this configuration.
 
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
 winston.configure({
+  format: combine(
+    timestamp(),
+    myFormat()
+  ),
   transports: [
     new winston.transports.File({ filename: 'cata.log' })
   ]
