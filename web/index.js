43ede7158cfd2c352582208068df50a6e62d436b
TyRoberts <t.roberts@livelinktechnology.net>
2019-12-07 19:25:52 +0000

    Pagination, and adding listings

--- web/index.js
+++ web/index.js
@@ -1,33 +1,64 @@
 const express = require("express");
 const path = require("path");
-const sqlite3 = require("sqlite3").verbose();
-const db = require('../cat_modules/db');
+const sqlite = require('../cat_modules/db');
+const bodyParser = require('body-parser');
 
 const { helpIcon } = require('../config.json');
 const catalogueSearch = require('../cat_modules/search_catalogue');
 
 const app = express();
-db.connect('../db/catalogue.db');
+sqlite.connect('../db/catalogue.db');
+const db = sqlite.load();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, () => console.log("Server started."));
 
 app.get("/", (req, res) => {
-  const sql = "SELECT rowid, * FROM listings ORDER BY rowid DESC LIMIT 10"
+  const page = parseInt(req.query.page) || 0;
+  const sql = `SELECT rowid, * FROM listings
+               WHERE "${knownActions.find(a => a === req.query.flag) || 'item'}"
+               LIKE "%${toSafeString(req.query.q)}%"
+               ORDER BY rowid DESC
+               LIMIT 10
+               OFFSET ${page * 10}`
+
   const searchListings = catalogueSearch.run(sql);
   searchListings.then((listings) => {
-    res.render("index", { listings, helpIcon });
+    const prev = page !== 0;
+    const next = listings.length === 10;
+    res.render("listings", { listings, helpIcon, page, next, prev });
   }).catch((err) => console.error(err))
 });
 
-app.get("/listings", (req, res) => {
-  const sql = `SELECT * FROM listings WHERE "${req.query.flag}" LIKE "%${req.query.q}%"`
-  console.log(sql)
-  const searchListings = catalogueSearch.run(sql);
-  searchListings.then((listings) => {
-    res.render("listings", { listings, helpIcon });
-  }).catch((err) => console.error(err))
+app.get("/add", (req, res) => {
+  res.render("add", { helpIcon });
 });
+
+app.post("/add-listing", (req, res) => {
+  const { user, primary, secondary, optional } = req.body;
+  const sql = `INSERT INTO listings (seller, item, price, location)
+               VALUES ("${toSafeString(user)}",
+                       "${toSafeString(primary)}",
+                       "${toSafeString(secondary)}",
+                       "${toSafeString(optional)}")`
+  db.run(sql, function(err) {
+    if (err) {
+      if (err.code === 'SQLITE_CONSTRAINT') {
+        return res.status(400).send("You're already selling that!");
+      } else {
+        return res.status(400).send('Something went wrong.');
+      }
+    }
+    return res.status(200).send('Added');
+  });
+});
+
+function toSafeString(str) {
+  return str ? str.replace(/[^ \w-@()\[\]_#,\.']+/g, '').trim() : '';
+}
+
+const knownActions = ['item', 'seller', 'location'];
--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,7 +1,32 @@
 window.onload = () => {
   $('.search-focus a').click(function () {
     const flag = $(this).attr('data-focus');
-    $('#flag').val(flag)
-    $('.main-search').attr('placeholder', `Search catalogue by ${flag}..`);
-  })
+    $('#flag').val(flag);
+    $('.main-search').attr('placeholder', `Search catalogue by ${flag}...`);
+  });
+
+  $('.pag').click(function() {
+    changePage($(this).attr('id'));
+  });
+
+  $('#add-listing').submit(function (evt) {
+    evt.preventDefault();
+    const form = $(this);
+    $.post(form.attr('action'), form.serialize())
+      .done( (e) => $('.alert').text(e) )
+      .fail( (xhr, textStatus, errorThrown) => $('.alert').text(xhr.responseText) );
+  });
 };
+
+function changePage(direction) {
+  const urlParams = new URLSearchParams(window.location.search);
+  const page = parseInt(urlParams.get('page')) || 0;
+
+  if (direction === 'next') {
+    urlParams.set('page', page + 1);
+  } else {
+    urlParams.set('page', page - 1);
+  }
+  
+  window.location.search = urlParams.toString()
+}
