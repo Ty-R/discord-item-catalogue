c1fd76e7f68f5c048aac944a8d4c3cabf8a4ff57
TyRoberts <t.roberts@livelinktechnology.net>
2020-02-02 11:30:48 +0000

    Remove embedded help information

--- commands/help.js
+++ commands/help.js
@@ -1,32 +1,9 @@
 module.exports = {
   name: 'help',
   execute(message, catalogue, args) {
-    const { helpIcon } = require('../config.json');
+    const description = require('../help.json');
 
-    message.channel.send({
-      "content": "Here's some basic information about me",
-      "embed": {
-        "description":"Catalogue is a bot designed to make it easier to keep track of who is selling what. It allows sellers to add listings, and buyers to query them. Click [here](https://github.com/TyRoberts/discord-item-catalogue#discord-catalogue-bot) for more information on usage.",
-        "color":3447003,
-        "thumbnail": {
-          "url": helpIcon
-        },
-        "fields": [
-          {
-            "name": "Example: Adding an item to the catalogue",
-            "value": "`!cat add 1 beacon:32 diamonds @spawn`"
-          },
-          {
-            "name": "Example: Removing an item in the catalogue",
-            "value": "`!cat remove 1 beacon`"
-          },
-          {
-            "name": "Example: Searching the catalogue for an item",
-            "value": "`!cat search beacon`"
-          }
-        ]
-      }
-    });
+    message.channel.send(description);
   },
 
   valid(args) {
